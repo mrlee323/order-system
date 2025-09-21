7a13c03244b2bc31d40f2e31a9c2c0bb51d69fcc
mrlee323 <mrlee3233@gmail.com>
2025-09-22 02:41:47 +0900

    feat: 메뉴 가격 계산 공통 함수 생성

---
+++ src/lib/utils/priceCalculator.ts
@@ -0,0 +1,36 @@
+import { MenuItem } from "@/lib/types/menu";
+import { SelectedOption } from "@/lib/types/cart";
+
+export const calculateTotalPrice = (
+  menuItem: MenuItem,
+  selectedOptions: SelectedOption[],
+  quantity: number
+): number => {
+  let basePrice = menuItem.price || 0;
+
+  let optionalPrice = 0;
+  let extrasPrice = 0;
+
+  selectedOptions.forEach((option) => {
+    const groupId = Number(option.groupId);
+    const choicePrice = option.choice?.price || 0;
+    const choiceQuantity = option.quantity || 0;
+
+    const isOptional = [
+      ...(menuItem.options?.optional || []),
+      ...(menuItem.options?.required || []),
+    ].some((opt) => opt.id === groupId);
+
+    const isExtras = [...(menuItem.options?.extras || [])].some(
+      (ext) => ext.id === groupId
+    );
+
+    if (isOptional) {
+      optionalPrice += choicePrice * choiceQuantity;
+    } else if (isExtras) {
+      extrasPrice += choicePrice * choiceQuantity;
+    }
+  });
+
+  return (basePrice + optionalPrice) * quantity + extrasPrice;
+};
