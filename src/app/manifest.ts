19323c9de74fc325b39986ebd004f3485c5f54ba
mrlee323 <mrlee3233@gmail.com>
2025-09-25 17:27:39 +0900

    fix: meta 정보 오류 수정

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -10,17 +10,6 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: "#ffffff",
     theme_color: "#2563eb",
     orientation: "portrait",
-    icons: [
-      {
-        src: "/icons/icon-192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/icons/icon-512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
-    ],
+    icons: [],
   };
 }
