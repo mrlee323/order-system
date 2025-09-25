fe5a4f332c5390943f1a4bf3ff3e8efd36618f42
mrlee323 <mrlee3233@gmail.com>
2025-09-25 21:50:21 +0900

    refactor: 반응형 사이즈별 카테고리 사이즈 조정

--- src/components/menu/MenuList.tsx
+++ src/components/menu/MenuList.tsx
@@ -84,33 +84,58 @@ export default function MenuList({
 
   if (!data) return null;
 
+  const SubCateory = () => {
+    return (
+      <div className="bg-white w-full py-2 sm:py-3 shadow-md">
+        <div className="flex gap-2 overflow-x-auto scrollbar-hide px-4 sm:px-6">
+          {subCategories.map((sub) => (
+            <button
+              key={sub.id}
+              type="button"
+              aria-label={sub.title + "서브카테고리 선택"}
+              className={`text-sm sm:text-base md:text-lg flex-shrink-0 px-4 py-2 rounded-full font-medium transition-all duration-200 whitespace-nowrap ${
+                selectedSubCategory === sub.type ||
+                (!selectedSubCategory && sub.id === subCategories[0].id)
+                  ? "bg-gradient-to-r from-blue-500 to-indigo-500 text-white shadow-md"
+                  : "bg-gray-100 text-gray-600 hover:bg-gray-200"
+              }`}
+              onClick={() => handleSubCategorySelect(sub.type)}
+            >
+              {sub.title}
+            </button>
+          ))}
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="flex portrait:flex-col landscape:flex-row h-screen bg-gradient-to-br from-slate-50 to-blue-50">
-      <div className="flex flex-col">
+      <div className="portrait:h-[17vh] flex flex-col portrait:fixed portrait:top-0 portrait:left-0 portrait:right-0 portrait:z-10 portrait:bg-white portrait:shadow-lg">
         {/* 스토어 헤더 */}
         {storeData && (
-          <div className="flex landscape:flex-col portrait:flex-row items-center gap-2 p-4 bg-white">
+          <div className="flex landscape:flex-col portrait:flex-row items-center gap-2 p-2 sm:p-4 bg-white">
             <Image
               src={storeData.logo || ""}
               alt={storeData.name || ""}
               width={50}
               height={50}
-              className="rounded-full object-cover"
+              className="rounded-full object-cover w-8 h-8 sm:w-12 sm:h-12 md:w-16 md:h-16 lg:w-20 lg:h-20 xl:w-24 xl:h-24"
             />
-            <div className="text-lg font-medium text-gray-700">
+            <div className="text-sm sm:text-base md:text-lg font-medium text-gray-700">
               {storeData.name}
             </div>
           </div>
         )}
 
         {/* 카테고리 */}
-        <div className="h-full flex gap-2 p-4 bg-white shadow-lg portrait:flex-row landscape:flex-col landscape:w-48 portrait:w-full portrait:border-y border-gray-100">
+        <div className="h-full flex gap-2 p-2 sm:p-4 bg-white shadow-lg portrait:flex-row landscape:flex-col landscape:w-48 portrait:w-full portrait:border-y border-gray-100">
           {categories.map((category) => (
             <button
               key={category.type}
               type="button"
               aria-label={category.title + "카테고리 선택"}
-              className={`landscape:w-full portrait:w-auto landscape:rounded-lg portrait:rounded-full text-left landscape:px-4 portrait:px-6 landscape:py-3 portrait:py-3 font-medium transition-all duration-200 ${
+              className={`text-sm sm:text-base md:text-lg landscape:w-full portrait:w-auto landscape:rounded-lg portrait:rounded-full text-left landscape:px-4 portrait:px-6 landscape:py-3  portrait:py-2 sm:portrait:py-3 font-medium transition-all duration-200 ${
                 selectedCategory === category.type
                   ? "bg-gradient-to-r from-blue-600 to-indigo-600 text-white shadow-lg transform landscape:scale-105"
                   : "text-gray-600 hover:bg-gray-100 hover:text-gray-800 portrait:shadow-md"
@@ -123,32 +148,21 @@ export default function MenuList({
             </button>
           ))}
         </div>
+
+        {/* 세로 모드 서브카테고리 */}
+        <div className="landscape:hidden">
+          <SubCateory />
+        </div>
       </div>
 
-      <div className="flex flex-col w-full">
-        {/* 서브카테고리 */}
-        <div className="bg-white w-full py-3 shadow-md">
-          <div className="flex gap-2 overflow-x-auto pb-2 scrollbar-hide px-6">
-            {subCategories.map((sub) => (
-              <button
-                key={sub.id}
-                type="button"
-                aria-label={sub.title + "서브카테고리 선택"}
-                className={`flex-shrink-0 px-4 py-2 rounded-full font-medium transition-all duration-200 whitespace-nowrap ${
-                  selectedSubCategory === sub.type ||
-                  (!selectedSubCategory && sub.id === subCategories[0].id)
-                    ? "bg-gradient-to-r from-blue-500 to-indigo-500 text-white shadow-md"
-                    : "bg-gray-100 text-gray-600 hover:bg-gray-200"
-                }`}
-                onClick={() => handleSubCategorySelect(sub.type)}
-              >
-                {sub.title}
-              </button>
-            ))}
-          </div>
+      <div className="flex flex-col w-full portrait:pt-[100px]">
+        {/* 가로 모드 서브카테고리 */}
+        <div className="portrait:hidden">
+          <SubCateory />
         </div>
+
         {/* 메뉴 리스트 */}
-        <div className="flex-1 overflow-y-auto p-6">
+        <div className="flex-1 overflow-y-auto p-6 portrait:pt-20 portrait:pb-20">
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
             {menuList?.items.map((item) => (
               <MenuCard
